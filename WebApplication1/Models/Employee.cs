cb86b831fef7a91f1a7a6a47ea56350646e89711
J Ben Anderson <revovisions@gmail.com>
2022-05-29 21:44:41 -0400

    added models department, employee and controllers

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Models
+{
+    public class Employee : Controller
+    {
+       public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+
+        public string DateOfJoining { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
+
